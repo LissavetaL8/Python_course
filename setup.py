2d58d656fe88c8fe62cc5c48e0fffead75cf101f
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-13 14:21:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ setup(
     version='0.0.1', # Версия вашей программы.
     packages=find_packages("."),
     scripts=["bin/uni_schedule.py"], # Расположение главного исполняемого файла.
-    url='https://github.com/...', # Адрес репозитория с вашей курсовой работой.
+    url='https://github.com/LissavetaL8/Python_course/tree/main', # Адрес репозитория с вашей курсовой работой.
     license='Apache-2.0',
-    author='...', # ФИО автора.
-    author_email='Лопатинская Елизавета Сергеевна', # Электронная почта автора.
+    author='Лопатинская Елизавета Сергеевна', # ФИО автора.
+    author_email='elizavetalopatinskaa@gmail.com', # Электронная почта автора.
     description='...', # Описание вашей поделки. Что она может, для чего сделана.
     include_package_data=True,
     install_requires=[],
 )
+
 
 
 
